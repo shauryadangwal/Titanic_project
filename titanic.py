17f76b2d4e0cf153e0a52716365b73cc5e67b948
shauryadangwal <42019930+shauryadangwal@users.noreply.github.com>
2020-07-19 21:56:25 +0530

    Add files via upload

---
+++ titanic.py
@@ -0,0 +1,223 @@
+#importpakages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#loading datasets
+train=pd.read_csv("C:\\Users\\Shaurya\\Desktop\\titanic_project\\train.csv")
+test=pd.read_csv("C:\\Users\\Shaurya\\Desktop\\titanic_project\\test.csv")
+print(train.describe(include="all"))
+print(train.columns)
+
+#check for any other unusable values
+print(pd.isnull(train).sum())
+
+#data Visualizzation
+#bar plot of survival by sex
+sns.barplot(x="Sex", y="Survived", data=train)
+
+#print percentages of females vs. males that survive
+print("Percentage of females who survived:", train["Survived"][train["Sex"] == 'female'].value_counts(normalize = True)[1]*100)
+
+print("Percentage of males who survived:", train["Survived"][train["Sex"] == 'male'].value_counts(normalize = True)[1]*100)
+
+
+#draw a bar plot of survival by Pclass
+sns.barplot(x="Pclass", y="Survived", data=train)
+
+#print percentage of people by Pclass that survived
+print("Percentage of Pclass = 1 who survived:", train["Survived"][train["Pclass"] == 1].value_counts(normalize = True)[1]*100)
+
+print("Percentage of Pclass = 2 who survived:", train["Survived"][train["Pclass"] == 2].value_counts(normalize = True)[1]*100)
+
+print("Percentage of Pclass = 3 who survived:", train["Survived"][train["Pclass"] == 3].value_counts(normalize = True)[1]*100)
+
+#draw a bar plot for SibSp vs. survival
+sns.barplot(x="SibSp", y="Survived", data=train)
+
+#I won't be printing individual percent values for all of these.
+print("Percentage of SibSp = 0 who survived:", train["Survived"][train["SibSp"] == 0].value_counts(normalize = True)[1]*100)
+
+print("Percentage of SibSp = 1 who survived:", train["Survived"][train["SibSp"] == 1].value_counts(normalize = True)[1]*100)
+
+print("Percentage of SibSp = 2 who survived:", train["Survived"][train["SibSp"] == 2].value_counts(normalize = True)[1]*100)
+
+#draw a bar plot for Parch vs. survival
+sns.barplot(x="Parch", y="Survived", data=train)
+plt.show()
+
+
+#sort the ages into logical categories
+train["Age"] = train["Age"].fillna(-0.5)
+test["Age"] = test["Age"].fillna(-0.5)
+bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf]
+labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+train['AgeGroup'] = pd.cut(train["Age"], bins, labels = labels)
+test['AgeGroup'] = pd.cut(test["Age"], bins, labels = labels)
+
+#draw a bar plot of Age vs. survival
+sns.barplot(x="AgeGroup", y="Survived", data=train)
+plt.show()
+
+train["CabinBool"] = (train["Cabin"].notnull().astype('int'))
+test["CabinBool"] = (test["Cabin"].notnull().astype('int'))
+
+#calculate percentages of CabinBool vs. survived
+print("Percentage of CabinBool = 1 who survived:", train["Survived"][train["CabinBool"] == 1].value_counts(normalize = True)[1]*100)
+
+print("Percentage of CabinBool = 0 who survived:", train["Survived"][train["CabinBool"] == 0].value_counts(normalize = True)[1]*100)
+#draw a bar plot of CabinBool vs. survival
+sns.barplot(x="CabinBool", y="Survived", data=train)
+plt.show()
+
+#test data
+
+#we'll start off by dropping the Cabin feature since not a lot more useful information can be extracted from it.
+train = train.drop(['Cabin'], axis = 1)
+test = test.drop(['Cabin'], axis = 1)
+
+#we can also drop the Ticket feature since it's unlikely to yield any useful information
+train = train.drop(['Ticket'], axis = 1)
+test = test.drop(['Ticket'], axis = 1)
+
+
+#now we need to fill in the missing values in the Embarked feature
+print("Number of people embarking in Southampton (S):")
+southampton = train[train["Embarked"] == "S"].shape[0]
+print(southampton)
+
+print("Number of people embarking in Cherbourg (C):")
+cherbourg = train[train["Embarked"] == "C"].shape[0]
+print(cherbourg)
+
+print("Number of people embarking in Queenstown (Q):")
+queenstown = train[train["Embarked"] == "Q"].shape[0]
+print(queenstown)
+
+#replacing the missing values in the Embarked feature with S
+train = train.fillna({"Embarked": "S"})
+
+#create a combined group of both datasets
+combine = [train, test]
+
+#extract a title for each Name in the train and test datasets
+for dataset in combine:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+pd.crosstab(train['Title'], train['Sex'])
+
+#replace various titles with more common names
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Capt', 'Col',
+    'Don', 'Dr', 'Major', 'Rev', 'Jonkheer', 'Dona'], 'Rare')
+    
+    dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean()
+#map each of the title groups to a numerical value
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Royal": 5, "Rare": 6}
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+train.head()
+# fill missing age with mode age group for each title
+mr_age = train[train["Title"] == 1]["AgeGroup"].mode() #Young Adult
+miss_age = train[train["Title"] == 2]["AgeGroup"].mode() #Student
+mrs_age = train[train["Title"] == 3]["AgeGroup"].mode() #Adult
+master_age = train[train["Title"] == 4]["AgeGroup"].mode() #Baby
+royal_age = train[train["Title"] == 5]["AgeGroup"].mode() #Adult
+rare_age = train[train["Title"] == 6]["AgeGroup"].mode() #Adult
+
+age_title_mapping = {1: "Young Adult", 2: "Student", 3: "Adult", 4: "Baby", 5: "Adult", 6: "Adult"}
+for x in range(len(train["AgeGroup"])):
+    if train["AgeGroup"][x] == "Unknown":
+        train["AgeGroup"][x] = age_title_mapping[train["Title"][x]]
+        
+for x in range(len(test["AgeGroup"])):
+    if test["AgeGroup"][x] == "Unknown":
+        test["AgeGroup"][x] = age_title_mapping[test["Title"][x]]
+
+#map each Age value to a numerical value
+age_mapping = {'Baby': 1, 'Child': 2, 'Teenager': 3, 'Student': 4, 'Young Adult': 5, 'Adult': 6, 'Senior': 7}
+train['AgeGroup'] = train['AgeGroup'].map(age_mapping)
+test['AgeGroup'] = test['AgeGroup'].map(age_mapping)
+
+train.head()
+
+#dropping the Age feature for now, might change
+train = train.drop(['Age'], axis = 1)
+test = test.drop(['Age'], axis = 1)
+#drop the name feature since it contains no more useful information.
+train = train.drop(['Name'], axis = 1)
+test = test.drop(['Name'], axis = 1)
+
+#map each Sex value to a numerical value
+sex_mapping = {"male": 0, "female": 1}
+train['Sex'] = train['Sex'].map(sex_mapping)
+test['Sex'] = test['Sex'].map(sex_mapping)
+
+train.head()
+#map each Embarked value to a numerical value
+embarked_mapping = {"S": 1, "C": 2, "Q": 3}
+train['Embarked'] = train['Embarked'].map(embarked_mapping)
+test['Embarked'] = test['Embarked'].map(embarked_mapping)
+
+train.head()
+
+#fill in missing Fare value in test set based on mean fare for that Pclass 
+for x in range(len(test["Fare"])):
+    if pd.isnull(test["Fare"][x]):
+        pclass = test["Pclass"][x] #Pclass = 3
+        test["Fare"][x] = round(train[train["Pclass"] == pclass]["Fare"].mean(), 4)
+        
+#map Fare values into groups of numerical values
+train['FareBand'] = pd.qcut(train['Fare'], 4, labels = [1, 2, 3, 4])
+test['FareBand'] = pd.qcut(test['Fare'], 4, labels = [1, 2, 3, 4])
+
+#drop Fare values
+train = train.drop(['Fare'], axis = 1)
+test = test.drop(['Fare'], axis = 1)
+#fitting model
+#splitting dataset
+from sklearn.model_selection import train_test_split
+
+predictors = train.drop(['Survived', 'PassengerId'], axis=1)
+target = train["Survived"]
+x_train, x_val, y_train, y_val = train_test_split(predictors, target, test_size = 0.22, random_state = 0)
+#random forest is used as it has good accuracy score
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+randomforest = RandomForestClassifier()
+randomforest.fit(x_train, y_train)
+y_pred = randomforest.predict(x_val)
+acc_randomforest = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_randomforest)
+
+#creating file 
+#set ids as PassengerId and predict survival 
+ids = test['PassengerId']
+predictions = randomforest.predict(test.drop('PassengerId', axis=1))
+
+#set the output as a dataframe and convert to csv file named submission.csv
+output = pd.DataFrame({ 'PassengerId' : ids, 'Survived': predictions })
+output.to_csv('output.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
